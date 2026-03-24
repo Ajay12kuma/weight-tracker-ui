551f0bc350193bb1e54ac93d82d3edf7a071be87
Ajay Mahule <ajaykumarmahule@gmail.com>
2026-03-24 21:31:04 +0530

    4rd changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.demo.weighttracker',
+  appName: 'Weight Tracker',
+  webDir: 'dist/weight-loss-tracker-ui/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
